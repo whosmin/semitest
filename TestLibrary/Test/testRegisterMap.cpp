ee805253c161e767c5963a2ef300a77c585bb140
tusharjog <tusharjog@7f14fe0c-a9dc-11de-a87c-5360b77f26dc>
2010-02-05 12:36:49 +0000

    minor changes

---
+++ TestLibrary/Test/testRegisterMap.cpp
@@ -0,0 +1,117 @@
+/*
+ * testRegisterMap.cpp
+ *
+ *      Author: tusharjog
+ */
+
+
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <gtest/gtest.h>
+
+using namespace std;
+
+#include "../Core/testLib.h"
+
+using namespace TestLib;
+
+
+//
+// Constructor Tests
+//
+TEST(RegisterMap, Constructor) {
+    //
+    //  Size upon construction
+    //
+    {
+        //
+        // RegisterMap Tests
+        //
+        RegisterMap dutRegMap("F761586");
+        cout << "dutRegMap.getSize()\t" << dutRegMap.getSize() << endl;
+
+        Register reg0( 8, "reg0", 0x00, "0xAA");
+        dutRegMap.addRegister(reg0);
+        cout << "dutRegMap.getSize()\t" << dutRegMap.getSize() << endl;
+        Register temp( 8);
+        temp = dutRegMap[0];
+
+        dutRegMap[0].print(cout);
+        dutRegMap.printDetailed(cout);
+    }
+    //
+    //  State upon construction
+    //
+    {
+    }
+    //
+    //  Default State upon construction
+    //
+    {
+    }
+
+}
+
+TEST( RegisterMap, AddRegister) {
+    //
+    //	
+    //
+    RegisterMap regMap("DUT");
+    EXPECT_EQ( regMap.getSize(), 0);
+
+    Register    reg0( 8, "reg0", 0x00, "0xAA");
+    EXPECT_EQ( regMap.addRegister(reg0), true);
+    cout << __func__ << regMap.getSize() << endl;
+
+    EXPECT_EQ( regMap.getSize(), 1);
+
+    cout << regMap[0] << endl;
+
+    regMap[0]      = 0x00; cout << regMap[0] << endl;
+    regMap["reg0"] = 0xFF; cout << regMap["reg0"] << endl;
+
+    EXPECT_EQ( regMap.addRegister(reg0), false);
+}
+TEST( RegisterMap, KeywordAccess) {
+    //
+    //	
+    //
+    RegisterMap regMap("DUT");
+    Register    reg0( 8, "reg0", 0x00, "0x00");
+    Register    reg1( 8, "reg1", 0x00, "0xFF");
+}
+
+TEST(RegisterMap, LValue_Assignment) {
+    //
+    // Tests for LValue assignment
+    //
+}
+
+TEST(RegisterMap,RValue_Assignment) {
+
+}
+
+TEST(Register, StringIndexing) {
+}
+
+TEST(Register, IntegerAssignmentOperator) {
+}
+
+
+TEST(Register, StringAssignmentOperator) {
+}
+
+int main(int argc, char** argv) {
+
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+
+#if 0
+
+#endif
+	
+
+	std::string input;
+	std::cin >> input;
+}
