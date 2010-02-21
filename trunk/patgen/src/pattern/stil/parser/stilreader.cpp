26c0548973b8d8e4681a1189d76cf7ae3a7a05cf
tusharjog <tusharjog@7f14fe0c-a9dc-11de-a87c-5360b77f26dc>
2010-02-21 21:57:23 +0000

    change wchar_t to char

--- trunk/patgen/src/pattern/stil/parser/stilreader.cpp
+++ trunk/patgen/src/pattern/stil/parser/stilreader.cpp
@@ -10,7 +10,7 @@ using namespace std;
 int main(int argc, char** argv) {
 
     if(argc > 1) {
-        wcout << "Reading file " << argv[1] << endl;
+        cout << "Reading file " << argv[1] << endl;
         FILE* fp = fopen( argv[1], "r");
         Scanner* scanner = new Scanner(fp);
         //wchar_t *filename = coco_string_create(argv[1]);
@@ -18,13 +18,13 @@ int main(int argc, char** argv) {
         Parser* parser   = new Parser(scanner);
         parser->Parse();
 
-        wcout << "Errors detected : " << parser->errors->count << endl;
+        cout << "Errors detected : " << parser->errors->count << endl;
 
         delete scanner;
         delete parser;
     }
     else
-        wcout << "No source file specified" << endl;
+        cout << "No source file specified" << endl;
 
     return 0;
 }
