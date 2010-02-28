9ffb1d9df0770c38e3c22c45191bb8ae6648a650
tusharjog <tusharjog@7f14fe0c-a9dc-11de-a87c-5360b77f26dc>
2010-02-28 15:44:02 +0000

    stil parser changes

--- patgen/src/pattern/stil/parser/Stil.cpp
+++ patgen/src/pattern/stil/parser/Stil.cpp
@@ -1,32 +1,65 @@
 
 #include "Stil.h"
+#include <iostream>
 
 namespace Stil {
 
-    Signal::Signal() {
-        _type = PSEUDO;
+    SymbolTable::SymbolTable() {
     }
 
-	Signal::Signal(string name, Type type) : _name(name), _type(type) {
+    bool SymbolTable::add(Object& obj) {
+        bool result = true;
+
+        if(exists(obj.getName()))
+            return false;
+
+        return result;
+    }
+#if 0
+    bool SymbolTable::add(string name, string scope) {
+        bool result = true;
+
+        return result;
+    }
+#endif
+
+    bool SymbolTable::exists(string name) {
+        bool result = false;
+
+        return result;
+    }
+
+    Signal::Signal(string name) : Object(STIL_SIGNAL) {
+        _name = name;
+        _subType = PSEUDO;
+    }
+
+	Signal::Signal(string name, SignalType type) : Object(STIL_SIGNAL), _subType(type) {
+        _name = name;
 	}
 
 
-	Signal::Signal(string name, string type) : _name(name) {
-        if(type == "InOut")
-            _type = INOUT;
-        else if(type == "Out")
-            _type = OUTPUT;
-        else if(type == "In")
-            _type = INPUT;
-        else if(type == "Supply")
-            _type = SUPPLY;
-        else if(type == "Pseudo")
-            _type = PSEUDO;
-        else
-            _type = PSEUDO;
+	Signal::Signal(string name, string type) : Object(STIL_SIGNAL) {
+        _name = name;
+
+        if(type == "InOut") _subType = INOUT;
+        else if(type == "Out") _subType = OUTPUT;
+        else if(type == "In") _subType = INPUT;
+        else if(type == "Supply") _subType = SUPPLY;
+        else if(type == "Pseudo") _subType = PSEUDO;
+        else _subType = PSEUDO;
 	}
 
-	Signals::Signals() {
+    void Signal::setSignalType( string type) {
+        if(type == "InOut") _subType = INOUT;
+        else if(type == "Out") _subType = OUTPUT;
+        else if(type == "In") _subType = INPUT;
+        else if(type == "Supply") _subType = SUPPLY;
+        else if(type == "Pseudo") _subType = PSEUDO;
+        else ;
+    }
+
+	Signals::Signals() : Object(STIL_SIGNALS) {
 	}
 
 	bool Signals::addSignal(Signal signal) {
@@ -38,13 +71,62 @@ namespace Stil {
 		else
 			result = false;
 
+        SymbolTable symTab = SymbolTable::getReference();
+        symTab.add(signal);
+
 		return result;
 	}
 
-    bool Signals::addSignal(string name, Signal::Type type) {
+    bool Signals::addSignal(string name, SignalType type) {
         Signal signal( name, type);
 
         return addSignal(signal);
+    }
+
+    string GroupsItem::toStil() {
+        string str;
+
+        str += _name + " = " + expr + "\n";
+
+        return str;
+    }
+
+    SignalGroups::SignalGroups() : Object(STIL_SIGNALGROUPS) {
+    }
+
+    string SignalGroups::toStil() {
+        string str;
+
+        str += "SignalGroups " + getName() + " {\n";
+        string indent = "\t";
+        map<string, GroupsItem>::iterator iter;
+        for(iter = groups.begin(); iter != groups.end(); iter++) {
+            str += indent + (*iter).second.toStil();
+        }
+        str += "}\n";
+        
+        return str;
+    }
+
+    bool SignalGroups::add( string name, string expr) {
+        bool result = true;
+
+        SymbolTable& symTab = SymbolTable::getReference();
+        if(symTab.exists(name))
+            return false;
+
+        GroupsItem gi;
+        gi.setName(name);
+        gi.expr = expr;
+
+        groups[name] = gi;
+
+        symTab.add(gi);
+
+        return result;
+    }
+
+    Spec::Spec() : Object(STIL_SPEC) {
     }
 
 }
